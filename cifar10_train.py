ef97fffbdcffc23285a203e3a3f0130d07e7e5e3
hesham mohamed <hesham52007@hotmail.com>
2017-10-08 02:07:57 +0200

    My commit message

--- cifar10_train.py
+++ cifar10_train.py
@@ -51,7 +51,7 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('train_dir', '/output',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 7500,
+tf.app.flags.DEFINE_integer('max_steps', 100000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
